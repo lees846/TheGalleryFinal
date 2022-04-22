183c146111134000f4964b6e2b5105cd05713859
Shayla L <lees846@newschool.edu>
2022-04-21 21:14:24 -0400

    Fixed S1 Frame origins to rotate on base pivot

---
+++ TheGalleryFinal2022/Assets/Scripts/openFrame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This script will make the image tilt forward so it's open at 29 seconds
+// It will initialize a new object in the frame
+// The new object sends user to next scene when collided with
+
+public class openFrame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
