9bc7a7eb0dae1c4e886f6765812bc49e206d1feb
Shayla L <lees846@newschool.edu>
2022-05-04 11:39:23 -0400

    Last scene fade animation and auto restart

--- TheGalleryFinal2022/Assets/Scripts/fadeOut.cs
+++ TheGalleryFinal2022/Assets/Scripts/fadeOut.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class fadeOut : MonoBehaviour
 {
 
     public Animator UIFade;
+    public string sceneToLoad;
     public string AnimationToPlay;
     AudioSource thisSongClip;
 
@@ -15,21 +17,24 @@ public class fadeOut : MonoBehaviour
         StartCoroutine(CountingSeconds());
     }
 
-    IEnumerator                                                                                                                                                                                                                                                                                      CountingSeconds()
+    IEnumerator CountingSeconds()
     {
         // wait until audiosource reaches boom sound
-        while (thisSongClip.time <= 23)
+        while (thisSongClip.time <= 23.5f)
         {
             yield return null;
         } 
 
         // start fade
         FadeToBlack();
-        Debug.Log(thisSongClip.time);
+        // Debug.Log(thisSongClip.time);
+
+        yield return new WaitForSeconds(16f);
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     void FadeToBlack()
     {
-        UIFade.SetTrigger(AnimationToPlay);
+        UIFade.Play(AnimationToPlay);
     }
 }
